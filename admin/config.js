9b2b8ae014393f3cce7dd590bec0f4e28a885dae
codecheap2019 <codecheap2019@gmail.com>
2020-04-30 19:24:43 +0530

    Firebase Setup

---
+++ admin/config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBth-CKjjINtT9_JxoaIGsdNSJt2tT9RR4",
+  authDomain: "exam-system-2020.firebaseapp.com",
+  databaseURL: "https://exam-system-2020.firebaseio.com",
+  projectId: "exam-system-2020",
+  storageBucket: "exam-system-2020.appspot.com",
+  messagingSenderId: "343303365852",
+  appId: "1:343303365852:web:ba7c9ed1dbbd2a86134019",
+  measurementId: "G-GRRRKW2E0T"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
